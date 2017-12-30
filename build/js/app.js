d52d72a685e4e9eac3e8d2205258f413d29aa0cc
Aaron Ross <superhawk610@gmail.com>
2017-12-30 15:02:43 -0500

    added error notification when channel fails to load iframe

--- build/js/app.js
+++ build/js/app.js
@@ -3,13 +3,19 @@ function notify(opts) {
   if (notifyIsActive) return
   notifyIsActive = true
   var title = opts.title || '',
-      msg = opts.message || '',
-      style = opts.style != '' ? `toast-${opts.style}` : '',
-      full = opts.fullWidth || false,
-      dur = opts.duration || 3000
-  $('#toast').find('.title').text(title).end()
-             .find('.text').text(msg).end()
-             .addClass(`${style}${full ? ' full-width' : ''}`).fadeIn()
+    msg = opts.message || '',
+    style = opts.style != '' ? `toast-${opts.style}` : '',
+    full = opts.fullWidth || false,
+    dur = opts.duration || 3000
+  $('#toast')
+    .find('.title')
+    .text(title)
+    .end()
+    .find('.text')
+    .text(msg)
+    .end()
+    .addClass(`${style}${full ? ' full-width' : ''}`)
+    .fadeIn()
   setTimeout(function() {
     $('#toast').fadeOut(500, function() {
       $('#toast').removeClass(`${style} full-width`)
@@ -17,3 +23,33 @@ function notify(opts) {
     })
   }, dur)
 }
+
+function displayError(message) {
+  $('#error')
+    .append(
+      `<div>
+        <span>Error!</span>
+        ${message}
+      </div>`
+    )
+    .show()
+}
+
+function checkFrame(frame) {
+  var html,
+    failed = false
+  try {
+    var doc = frame.contentDocument || frame.contentWindow.document
+    html = doc.body.innerHTML
+  } catch (e) {
+    failed = true
+  }
+
+  if (!html) failed = true
+  if (failed)
+    displayError('Frame loading blocked due to cross-origin prevention')
+}
+
+$('iframe').each(function(i) {
+  checkFrame(this)
+})
