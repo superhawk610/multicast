d9d43c3dcf9743208bce877ed70e75436140a005
Aaron Ross <superhawk610@gmail.com>
2019-03-05 21:29:47 -0500

    project scaffolding

---
+++ client/src/__tests__/index.test.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import test from 'ava';
+
+test('sanity', t => {
+  t.truthy(<div>I'm not even a bit insane</div>);
+});
---
+++ server/src/__tests__/index.test.ts
@@ -0,0 +1,5 @@
+import test from 'ava';
+
+test('sanity', t => {
+  t.truthy("I'm not insane");
+});
