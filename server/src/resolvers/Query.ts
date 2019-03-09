0da1ea31c8f7a3a9206c37ac81c2b98a04a12ce2
Aaron Ross <superhawk610@gmail.com>
2019-03-09 00:58:16 -0500

    feat: implement Query resolvers

--- server/src/resolvers/Query.ts
+++ server/src/resolvers/Query.ts
@@ -1,27 +1,30 @@
+import { getActiveChannel } from '../services/takeover.service';
+
 import Device from '../models/device.model';
 import Channel from '../models/channel.model';
 import Alert from '../models/alert.model';
 
 export const Query = {
-  device(_, { id }: { id: string }) {
+  device(_, { id }) {
     return Device.findByPk(id);
   },
   devices() {
     return Device.findAll();
   },
-  channel() {
-    return null;
+  channel(_, { id }) {
+    return Channel.findByPk(id);
   },
   channels() {
     return Channel.findAll();
   },
-  alert() {
-    return null;
+  alert(_, { id }) {
+    return Alert.findByPk(id);
   },
   alerts() {
     return Alert.findAll();
   },
   takeover() {
-    return { active: false, channel: null };
+    const channel = getActiveChannel();
+    return { active: Boolean(channel), channel };
   },
 };
