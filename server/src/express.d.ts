f8791f3415e7d6217b1a7cbbad836d0fec12aa66
Aaron Ross <superhawk610@gmail.com>
2019-06-13 22:12:21 -0400

    fix(server): fix failing TS build

--- server/src/express.d.ts
+++
@@ -1,5 +0,0 @@
-declare namespace Express {
-  export interface Request {
-    clientIp: string;
-  }
-}
--- server/src/routers/client.router.ts
+++ server/src/routers/client.router.ts
@@ -1,8 +1,12 @@
 import { join } from 'path';
 import { readFileSync } from 'fs';
-import { Router, Request } from 'express';
+import { Router, Request as ExpressRequest } from 'express';
 
 import Device from '../models/device.model';
+
+interface Request extends ExpressRequest {
+  clientIp: string;
+}
 
 const router = Router();
 
@@ -14,21 +18,23 @@ router.get('*', (req, res) => {
   if (path.indexOf('.') > -1) {
     res.sendFile(join(publicDir, path));
   } else {
-    Device.findOne({ where: { address: req.clientIp } }).then(device => {
-      const html = readFileSync(index, 'utf-8');
-      res.set('content-type', 'text/html');
-      res.send(
-        html
-          .replace('"#INJECT_ACTIVE"', 'true')
-          .replace('"#INJECT_DEVICE"', device ? `"${device.id}"` : 'null')
-          .replace('#INJECT_HOST', req.clientIp)
-          .replace(
-            '#INJECT_NAME',
-            device ? device.nickname : 'Unrecognized Device',
-          )
-          .replace('#INJECT_UPSTREAM', 'localhost:4000'),
-      );
-    });
+    Device.findOne({ where: { address: (req as Request).clientIp } }).then(
+      device => {
+        const html = readFileSync(index, 'utf-8');
+        res.set('content-type', 'text/html');
+        res.send(
+          html
+            .replace('"#INJECT_ACTIVE"', 'true')
+            .replace('"#INJECT_DEVICE"', device ? `"${device.id}"` : 'null')
+            .replace('#INJECT_HOST', (req as Request).clientIp)
+            .replace(
+              '#INJECT_NAME',
+              device ? device.nickname : 'Unrecognized Device',
+            )
+            .replace('#INJECT_UPSTREAM', 'localhost:4000'),
+        );
+      },
+    );
   }
 });
 
