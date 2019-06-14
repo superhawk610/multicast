db293e4a2510f3d8cadcbeedc01cff74527c93ea
Aaron Ross <superhawk610@gmail.com>
2019-06-14 02:31:25 -0400

    chore(server): bump ts-jest to ^24.0.2

--- server/src/services/__tests__/get-data-directory.service.test.ts
+++ server/src/services/__tests__/get-data-directory.service.test.ts
@@ -1,5 +1,4 @@
-// FIXME: update `ts-jest` once https://github.com/kulshekhar/ts-jest/pull/994 lands
-// import { mocked } from 'ts-jest/utils';
+import { mocked } from 'ts-jest/utils';
 import { getDataDirectory } from '../get-data-directory.service';
 import { mkdirSync, existsSync } from 'fs';
 
@@ -14,17 +13,17 @@ jest.mock('../config.service', () => ({
 
 describe('getDataDirectory', () => {
   afterEach(() => {
-    (mkdirSync as any).mockClear();
+    mocked(mkdirSync).mockClear();
   });
 
   it("creates the directory if it doesn't exist", () => {
-    (existsSync as any).mockImplementation(() => false);
+    mocked(existsSync).mockImplementation(() => false);
     getDataDirectory();
     expect(mkdirSync).toBeCalled();
   });
 
   it('just returns the path if the directory already exists', () => {
-    (existsSync as any).mockImplementation(() => true);
+    mocked(existsSync).mockImplementation(() => true);
     getDataDirectory();
     expect(mkdirSync).not.toBeCalled();
   });
