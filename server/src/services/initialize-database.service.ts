35e3eaa128718def236281143e62a3767dad9650
Aaron Ross <superhawk610@gmail.com>
2019-10-19 23:43:01 -0400

    feat(server): maintain separate DBs for sandbox/non-sandbox envs

--- server/src/services/initialize-database.service.ts
+++ server/src/services/initialize-database.service.ts
@@ -1,4 +1,5 @@
 import { join } from 'path';
+import { unlinkSync } from 'fs';
 import { Sequelize } from 'sequelize-typescript';
 
 import { getDataDirectory } from './get-data-directory.service';
@@ -6,17 +7,30 @@ import { getDataDirectory } from './get-data-directory.service';
 import { getConfig } from './config.service';
 
 export async function initializeDatabase() {
-  const { SQL_LOGGING } = getConfig();
+  const { SANDBOX, SQL_LOGGING } = getConfig();
   const dataDirectory = getDataDirectory();
+  const storage = join(dataDirectory, SANDBOX ? 'sandbox.sqlite3' : 'db.sqlite3');
+
+  // remove existing sandbox database from previous run, if it exists
+  if (SANDBOX) {
+    try {
+      unlinkSync(storage);
+    } catch (e) {}
+  }
+
+  // initialize database connection
   const db = new Sequelize({
     logging: SQL_LOGGING,
     database: 'multicast',
     dialect: 'sqlite',
     username: 'root',
     password: '',
-    storage: join(dataDirectory, 'db.sqlite3'),
+    storage,
     modelPaths: [join(__dirname, '..', 'models', '*.model.ts')],
   });
+
+  // create database schema if it doesn't already exist
   await db.sync();
+
   return db;
 }
